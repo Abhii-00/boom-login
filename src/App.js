8320bbcf52352f3bda3195dd604d2cb0c063220a
Abhii-00 <abhijithedayate779@gmail.com>
2025-08-02 14:05:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -331,7 +331,7 @@ function App() {
     <>
       <div className="video-background">
         <video autoPlay loop muted playsInline>
-          <source src="./public/background-video.mp4" type="video/mp4" />
+          <source src="/background-video.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
       </div>
